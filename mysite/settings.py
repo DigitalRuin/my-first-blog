327a6fea2d60bc37e458ed3142c605d5fc2531c3
Bill <wpr2000@yahoo.com>
2018-01-11 13:57:02 -0500

    My Totorial App, commit 2

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'lagpulyox!tnwy9-5ln-ytk0&%hb+(_#umx6k!&ou-cpmd94@!'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'ureynwi.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'DigitalRuin.pythonanywhere.com']
 
 
 # Application definition
